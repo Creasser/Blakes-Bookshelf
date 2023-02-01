ad7f65ac6e28f832ef9d907361c48425d4ce076c
Blake Creasser <blakecreasser@aggienetwork.com>
2023-01-31 22:18:45 -0600

    add psudeo code, setup db.json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+//create NavBar component that will have links to the Home page, book page, and add book page
+//create Home component, this component will just have general information about the website
+//create BookShelf component: This component will display a small preview of all the books. When the book is clicked on, the user will be redirected to another page that shows only the book clicked on and more information about the book.
+//will need to make a book component that can be resused for each book that is passed to create the book card
+
 function App() {
   return (
     <div>
