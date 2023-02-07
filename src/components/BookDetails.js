d30b3900d5c4253116539f50dc85d385308ef57b
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-06 21:51:28 -0600

    add JSX to bookdetails component

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BookDetails(){
+const [book, setBook] = useState(null)
+
+
 
     return(
         <div>
-
+            <h1>{book.title}</h1>
+            <img src={book.image}></img>
+            <h4>{book.author}</h4>
+            <p>{book.summary}</p>
+            <p>{`Pages: ${book.pageCount}`}</p>
+            <a href={book.link}>Click Here to Purchase</a>
         </div>
     )
 }
