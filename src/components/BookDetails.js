3f69b2176b8adca669431a8b1104f8a81baf8974
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-08 14:35:50 -0600

    style changes

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -14,15 +14,15 @@ useEffect(() => {
 }, [params.id])
 
 
-if (!book) return <h1>Loading...</h1>
+if (!book) return <h1 className="loading">Loading...</h1>
 
     return(
         <div className="bookDetails">
             <h1>{book.title}</h1>
             <img src={book.image}></img>
             <h2>{book.author}</h2>
-            <p id="summary">{book.summary}</p>
             <p>{`Pages: ${book.pageCount}`}</p>
+            <p id="summary">{book.summary}</p>
             <a className="bookLinks" href={book.link}>Click Here to Purchase</a>
             <br />
             <Link className="bookLinks" to="/bookshelf">Return to Bookshelf</Link>
