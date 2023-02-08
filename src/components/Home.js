4b395a69d8fc1e95438b479af0eaec53b202d5cb
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-07 21:48:48 -0600

    update homepage text, format homepage elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ function Home(){
 
     return (
         <div className="homepage">
-            <h1 id="title">Welcome to your Bookshelf!</h1>
-            <div>This application has some of my favorite books added to get you started! Feel free to add as many books as you would like!</div>
+            <h1 id="title">Digital Bookshelf!</h1>
+            <div className="home">Welcome to the digital bookcase, the ultimate solution for book lovers everywhere! With this web application, you can now keep a record of all the books you own, read, and want to read, in one convenient and organized digital space. Say goodbye to cluttered bookshelves and forgetfulness, and never miss a beat in your reading journey. This easy-to-use platform allows you to add books to your collection with just a few clicks. Whether you're a fan of best-selling novels, classic literature, or non-fiction, you can keep track of your entire library with ease. Keep your books organized, never forget what you've read, and never run out of books to add to your list!</div>
         </div>
     )
 }
